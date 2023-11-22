07d9550dc29a7d8eba4596f16b5808d56d286a3a
bolexs <boluwatufeadeojo@gmail.com>
2023-11-22 20:25:14 +0100

    adding boiler files for frontend and making adjustments to settings.py file.

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'web_static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
