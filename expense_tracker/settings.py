98d754ad41604d3f3cb2ba7808376d182c33ad52
babafemiolatona <femiolatona@gmail.com>
2023-12-12 20:07:53 +0100

    Added leading slash

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'web_static/'
+STATIC_URL = '/web_static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'web_static'),
