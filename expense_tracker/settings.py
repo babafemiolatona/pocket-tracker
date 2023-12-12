867cfc548774c1c1ded35f239991e013e7ac44ae
babafemiolatona <femiolatona@gmail.com>
2023-12-12 23:14:08 +0100

    Added MIME type for .js files

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/web_static/'
+STATIC_URL = 'web_static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'web_static'),
@@ -156,6 +156,6 @@ EMAIL_HOST_PASSWORD = config("EMAIL_PASSWORD") # App password generated from gma
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 
-# import mimetypes
+import mimetypes
 
-# mimetypes.add_type("text/javascript", ".js", True)
+mimetypes.add_type("text/javascript", ".js", True)
